ed303253593b4f579ec05c5546727eeccf717a21
Rezor <kostiatsymbal2019@gmail.com>
2024-09-19 13:43:47 +0300

    Merging almost done.

--- main.cpp
+++ main.cpp
@@ -12,14 +12,8 @@ constexpr int MAX_INT = 100;
 int main() {
     const std::string inputFile = "../files/A.bin";
     const std::string tempFile = "../files/temp.bin";
-    const std::vector<std::string> subFilesB{
-        "../files/B1.bin", "../files/B2.bin", "../files/B3.bin",
-        "../files/B4.bin", "../files/B5.bin", "../files/B6.bin"
-    };
-    const std::vector<std::string> subFilesC{
-        "../files/C1.bin", "../files/C2.bin", "../files/C3.bin",
-        "../files/C4.bin", "../files/C5.bin", "../files/C6.bin"
-    };
+    const std::vector<std::string> subFilesB{"../files/B1.bin", "../files/B2.bin", "../files/B3.bin"};
+    const std::vector<std::string> subFilesC{"../files/C1.bin", "../files/C2.bin", "../files/C3.bin"};
 
     {
         std::ofstream file(inputFile, std::ios::binary | std::ios::trunc);
@@ -41,27 +35,27 @@ int main() {
 
     std::cout << "Generating..." << std::endl;
     auto start = std::chrono::high_resolution_clock::now();
-    Generator::fileFileWithNumbers(inputFile, FILE_SIZE_MB, MIN_INT, MAX_INT);
-    // std::vector numbers = {3, 7, 2, 5, 10, 6, 1, 8, 9, 4, 7, 5, 2, 6, 9, 3, 8, 1, 10, 4, 5, 7, 2, 6, 9, 3, 8, 10, 1, 4};
-    // // #1 3 7 2 6 9 1 4
-    // // #2 2 5 10 3 8
-    // // #3 6 1 10
-    // // #4 1 8 9 4 5 7
-    // // #5 4 7 2 6 9
-    // // #6 5 3 8 10
-    // std::ofstream file(inputFile, std::ios::binary);
-    // file.write(reinterpret_cast<const char *>(numbers.data()), numbers.size() * sizeof(int));
-    // file.close();
+    //Generator::fileFileWithNumbers(inputFile, FILE_SIZE_MB, MIN_INT, MAX_INT);
+    std::vector numbers = {3, 7, 2, 5, 10, 6, 1, 8, 9, 4, 7, 5, 2, 6, 9, 3, 8, 1, 10, 4, 5, 7, 2, 6, 9, 3, 8, 10, 1, 4};
+    // #1 3 7 / 1 8 9 / 2 6 9 / 4 5 7 / 1 4
+    // #2 2 5 10 / 4 7 / 3 8 / 2 6 9
+    // #3 6 / 5 / 1 10 / 3 8 10
+    // 2 3 5 6 7 10 / 2 3 4 5 6 7 8 9 10
+    // 1 4 5 7 8 9 / 1 4
+    // 1 2 3 6 8 9 10
+    std::ofstream file(inputFile, std::ios::binary);
+    file.write(reinterpret_cast<const char *>(numbers.data()), numbers.size() * sizeof(int));
+    file.close();
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
     std::cout << "Operation took " << duration.count() << " seconds." << std::endl;
 
-    std::cout << "Sorting..." << std::endl;
-    start = std::chrono::high_resolution_clock::now();
-    FilesActions::sortFileBySection(inputFile, tempFile, SORT_PART_SIZE_MB);
-    end = std::chrono::high_resolution_clock::now();
-    duration = end - start;
-    std::cout << "Operation took " << duration.count() << " seconds." << std::endl;
+    // std::cout << "Sorting..." << std::endl;
+    // start = std::chrono::high_resolution_clock::now();
+    // FilesActions::sortFileBySection(inputFile, tempFile, SORT_PART_SIZE_MB);
+    // end = std::chrono::high_resolution_clock::now();
+    // duration = end - start;
+    // std::cout << "Operation took " << duration.count() << " seconds." << std::endl;
 
     std::cout << "Mergin..." << std::endl;
     start = std::chrono::high_resolution_clock::now();
